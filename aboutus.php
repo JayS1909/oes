230baa0ff42744f942180cee45869a08f80670e4
AcadmixHub <jayeshs1909@gmail.com>
2024-05-01 16:41:03 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>About Us</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700,300">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/font.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body style="background-image: url(image/bg1.jpg); background-size: cover;">
+
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">PBL Project</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        
+        <li><a href="feedback.php" target="_blank">Feedback</a></li>
+        <li><a href="#">Services</a></li>
+        
+      </ul>
+    </div>
+  </nav>
+</header>
+
+
+
+<main>
+  <section id="about-us">
+
+    <div class="container">
+      <h1>About Us</h1>
+      <p>We are a college project group of five members who have successfully completed our PBL project, an online examination system using PHP and JavaScript. We are all passionate about technology and education, and we are excited to share our project with the world.
+
+Our online examination system is designed to make it easy for students to take exams online. It is secure, reliable, and easy to use. Students can take exams at their own pace and from anywhere in the world.
+
+We are confident that our online examination system will be a valuable tool for students and educators alike. We are grateful for the opportunity to have worked on this project, and we look forward to seeing how it is used to improve education.</p>
+    </div>
+  </section>
+</main>
+
+<div class="row footer">
+<div class="col-md-3 box">
+<a href="index.php" target="_blank">Home</a>
+</div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#developers">Developers</a>
+</div>
+<div class="col-md-3 box">
+<a href="feedback.php" target="_blank">Feedback</a></div></div>
+<!-- Modal For Developers-->
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
+      </div>
+    
+      <div class="modal-body">
+        <p>
+    <div class="row">
+    <div class="col-md-4">
+     <img src="image/coffin.png" width=100 height=100 alt="PBL-A18" class="img-rounded">
+     </div>
+     <div class="col-md-5">
+    
+    <h4 style="font-family:'typo' ">KETAN.B</h4>
+    <h4 style="font-family:'typo' ">SE </h4>
+    <h4 style="font-family:'typo' ">TCOER , Pune .</h4></div></div>
+    </p>
+      </div>
+    
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div>
+
+</body>
+</html>
